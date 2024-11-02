90e5ed30280798c54a7d00eb2457b3d0c9bec228
pydevtech1 <gitbuhio@gmail.com>
2024-11-02 19:14:45 +0700

    Update js.js

--- about/js.js
+++ about/js.js
@@ -3,18 +3,3 @@ const scrollUpBtn = document.getElementById('scrollUpBtn');
 function scrollToTop(){
     window.scrollTo(0, 0);
 }
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah reload halaman
-
-    const username = this.username.value;
-    const password = this.password.value;
-
-    // Proses login, misalnya panggil API
-
-    // Jika login berhasil
-    document.getElementById('message').textContent = 'Login berhasil!';
-    
-    // Kosongkan input
-    this.username.value = '';
-    this.password.value = '';
-});
